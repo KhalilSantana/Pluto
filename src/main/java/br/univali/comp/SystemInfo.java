ffb94b4d94e695e32467207034fd73e68285a6f9
Khalil Santana <khalilsantana@hotmail.com>
2021-03-19 15:55:36 -0300

    Initial commit

---
+++ src/main/java/br/univali/comp/SystemInfo.java
@@ -0,0 +1,13 @@
+package br.univali.comp;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module br.univali.comp {
+    requires javafx.controls;
+    exports br.univali.comp;
+}
