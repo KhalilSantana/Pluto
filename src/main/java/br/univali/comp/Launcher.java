203b4a09c6784eac20d6676459b2a5f1a750cc5a
Khalil Santana <khalilsantana@hotmail.com>
2021-03-19 21:16:27 -0300

    Add .JAR packaging

---
+++ src/main/java/br/univali/comp/Launcher.java
@@ -0,0 +1,8 @@
+package br.univali.comp;
+
+public class Launcher {
+    // This is a workaround required for creating JavaFX apps in a single archive (.JAR)
+    public static void main(String[] args){
+        App.main(args);
+    }
+}
