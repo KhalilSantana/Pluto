9d6f1b079273871375c274bcfd7bfc2c622b4c0b
Khalil Santana <khalilsantana@hotmail.com>
2021-03-22 21:18:27 -0300

    UI: Change app window title Pluto -> Compilador

--- src/main/java/br/univali/comp/App.java
+++ src/main/java/br/univali/comp/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
         root.getStylesheets().add("/main.css");
-        primaryStage.setTitle("Pluto");
+        primaryStage.setTitle("Compilador");
         primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
     }
