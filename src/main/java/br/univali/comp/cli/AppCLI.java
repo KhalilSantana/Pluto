816fc2938f81e5a9d4b78aa0b2c747559bbccde2
Khalil Santana <khalilsantana@hotmail.com>
2021-05-07 00:54:33 -0300

    Create cli/AppCLI.java for manual testing
    To run this class run `mvn compile exec:java`

---
+++ src/main/java/br/univali/comp/cli/AppCLI.java
@@ -0,0 +1,22 @@
+package br.univali.comp.cli;
+
+import br.univali.comp.gui.EditorFile;
+import br.univali.comp.javacc.gen.ParseException;
+import br.univali.comp.javacc.gen.Sintatico;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AppCLI {
+    // Dummy class for manual testing
+
+    public static void main(String[] args) throws IOException, ParseException {
+        // This will analyze the file in Pluto/tests/hardcoded.txt
+        EditorFile editorFile = new EditorFile(new File("tests/hardcoded.txt"), false);
+        String fileContents = editorFile.getFileContents();
+        InputStream targetStream = new java.io.ByteArrayInputStream(fileContents.getBytes());
+        Sintatico s = new Sintatico(targetStream);
+        s.analyze(args, editorFile.getFileContents());
+    }
+}
