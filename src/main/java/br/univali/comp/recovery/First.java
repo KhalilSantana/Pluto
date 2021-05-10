1a45ea73ddc9f9577aafa1d313f11d954a9748fb
Khalil Santana <khalilsantana@hotmail.com>
2021-05-09 21:01:08 -0300

    feat: error recovery on define()

--- src/main/java/br/univali/comp/recovery/First.java
+++ src/main/java/br/univali/comp/recovery/First.java
@@ -6,10 +6,12 @@ import br.univali.comp.javacc.gen.*;
 public class First { //implementa os conjuntos first p/ alguns n.terminais
 
     static public final RecoverySet program = new RecoverySet();
+    public static RecoverySet define = new RecoverySet();
 
     static {
         //DEPOIS da configuração ajustar o próximo token
         //program.add(new Integer(SintaticoConstants.R_PROGRAM));
         program.add(new Integer(SintaticoConstants.EOF));
+        define.add(SintaticoConstants.R_EXECUTE);
     }
 }
