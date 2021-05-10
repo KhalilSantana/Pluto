f034bfb0775f0b97291242228a10991e391cd68e
Khalil Santana <khalilsantana@hotmail.com>
2021-05-09 21:49:09 -0300

    Add recovery error message for execute()

--- src/main/java/br/univali/comp/recovery/First.java
+++ src/main/java/br/univali/comp/recovery/First.java
@@ -7,11 +7,13 @@ public class First { //implementa os conjuntos first p/ alguns n.terminais
 
     static public final RecoverySet program = new RecoverySet();
     public static RecoverySet define = new RecoverySet();
+    public static RecoverySet execute = new RecoverySet();
 
     static {
         //DEPOIS da configuração ajustar o próximo token
         //program.add(new Integer(SintaticoConstants.R_PROGRAM));
         program.add(new Integer(SintaticoConstants.EOF));
         define.add(SintaticoConstants.R_EXECUTE);
+        execute.add(SintaticoConstants.SS_CURLYBRACE_CLOSE);
     }
 }
