bc7c54e7f9d83232aa3cf7121069cc8b6a0ea59c
Khalil Santana <khalilsantana@hotmail.com>
2021-05-09 23:35:28 -0300

    Add error messages for invalid expressao()

--- src/main/java/br/univali/comp/recovery/First.java
+++ src/main/java/br/univali/comp/recovery/First.java
@@ -8,12 +8,15 @@ public class First { //implementa os conjuntos first p/ alguns n.terminais
     static public final RecoverySet program = new RecoverySet();
     public static RecoverySet define = new RecoverySet();
     public static RecoverySet execute = new RecoverySet();
-
+    public static RecoverySet expressao = new RecoverySet();
     static {
         //DEPOIS da configuração ajustar o próximo token
         //program.add(new Integer(SintaticoConstants.R_PROGRAM));
         program.add(new Integer(SintaticoConstants.EOF));
         define.add(SintaticoConstants.R_EXECUTE);
         execute.add(SintaticoConstants.SS_CURLYBRACE_CLOSE);
+        // expressao
+        expressao.add(SintaticoConstants.R_IS);
+        expressao.add(SintaticoConstants.R_TO);
     }
 }
