a6d9a3f605c36b11ba7b5d9a370b4463121bb943
Khalil Santana <khalilsantana@hotmail.com>
2021-06-03 22:41:49 -0300

    tests: more tests for ADD

--- src/test/java/br/univali/comp/virtualmachine/VirtualMachineTest.java
+++ src/test/java/br/univali/comp/virtualmachine/VirtualMachineTest.java
@@ -31,4 +31,66 @@ class VirtualMachineTest {
         assertEquals(DataType.INTEGER, stackElement.type);
         assertEquals(expectedResult, (Integer) stackElement.content);
     }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+//            X    Y    ExpectedResult
+            " 1.0, 2.0, 3.0",
+            "-1.0, 1.0, 0.0"
+    })
+    @Name("ADD-Floats")
+    void addFloats(Float x, Float y, Float expectedResult) {
+        var insList = new ArrayList<Instruction>();
+        insList.add(new Instruction(Instruction.Mnemonic.LDR, new DataFrame(DataType.FLOAT, x)));
+        insList.add(new Instruction(Instruction.Mnemonic.LDR, new DataFrame(DataType.FLOAT, y)));
+        insList.add(new Instruction(Instruction.Mnemonic.ADD, new DataFrame(DataType.NONE, null)));
+        insList.add(new Instruction(Instruction.Mnemonic.STP, new DataFrame(DataType.NONE, null)));
+        var vm = new VirtualMachine(insList);
+        vm.executeAll();
+        var stack = vm.getStack();
+        assertEquals(1, stack.size());
+        var stackElement = stack.peek();
+        assertEquals(DataType.FLOAT, stackElement.type);
+        assertEquals(expectedResult, (Float) stackElement.content);
+    }
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+//           X    Y    ExpectedResult
+            "1,   2.0, 3.0",
+    })
+    @Name("ADD-IntegerFloat")
+    void addIntegerFloat(Integer x, Float y, Float expectedResult) {
+        var insList = new ArrayList<Instruction>();
+        insList.add(new Instruction(Instruction.Mnemonic.LDI, new DataFrame(DataType.INTEGER, x)));
+        insList.add(new Instruction(Instruction.Mnemonic.LDR, new DataFrame(DataType.FLOAT, y)));
+        insList.add(new Instruction(Instruction.Mnemonic.ADD, new DataFrame(DataType.NONE, null)));
+        insList.add(new Instruction(Instruction.Mnemonic.STP, new DataFrame(DataType.NONE, null)));
+        var vm = new VirtualMachine(insList);
+        vm.executeAll();
+        var stack = vm.getStack();
+        assertEquals(1, stack.size());
+        var stackElement = stack.peek();
+        assertEquals(DataType.FLOAT, stackElement.type);
+        assertEquals(expectedResult, (Float) stackElement.content);
+    }
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+//           X    Y  ExpectedResult
+            "1.0, 2, 3.0",
+    })
+    @Name("ADD-FloatInteger")
+    void addFloatInteger(Float x, Integer y, Float expectedResult) {
+        var insList = new ArrayList<Instruction>();
+        insList.add(new Instruction(Instruction.Mnemonic.LDR, new DataFrame(DataType.FLOAT, x)));
+        insList.add(new Instruction(Instruction.Mnemonic.LDI, new DataFrame(DataType.INTEGER, y)));
+        insList.add(new Instruction(Instruction.Mnemonic.ADD, new DataFrame(DataType.NONE, null)));
+        insList.add(new Instruction(Instruction.Mnemonic.STP, new DataFrame(DataType.NONE, null)));
+        var vm = new VirtualMachine(insList);
+        vm.executeAll();
+        var stack = vm.getStack();
+        assertEquals(1, stack.size());
+        var stackElement = stack.peek();
+        assertEquals(DataType.FLOAT, stackElement.type);
+        assertEquals(expectedResult, (Float) stackElement.content);
+    }
 }
