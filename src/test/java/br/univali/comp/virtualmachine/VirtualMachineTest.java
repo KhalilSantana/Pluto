9bb19f42ba6cbcb466b54ec3c5d7e7a5f3db3fb0
Khalil Santana <khalilsantana@hotmail.com>
2021-06-03 22:03:43 -0300

    feat: include unit tests for vm-exec instructions

---
+++ src/test/java/br/univali/comp/virtualmachine/VirtualMachineTest.java
@@ -0,0 +1,34 @@
+package br.univali.comp.virtualmachine;
+
+import jdk.jfr.Name;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class VirtualMachineTest {
+
+    @ParameterizedTest
+    @CsvSource({
+//            X  Y  ExpectedResult
+            " 1, 2, 3",
+            "-1, 1, 0"
+    })
+    @Name("ADD-Integers")
+    void addIntegers(Integer x, Integer y, Integer expectedResult) {
+        var insList = new ArrayList<Instruction>();
+        insList.add(new Instruction(Instruction.Mnemonic.LDI, new DataFrame(DataType.INTEGER, x)));
+        insList.add(new Instruction(Instruction.Mnemonic.LDI, new DataFrame(DataType.INTEGER, y)));
+        insList.add(new Instruction(Instruction.Mnemonic.ADD, new DataFrame(DataType.NONE, null)));
+        insList.add(new Instruction(Instruction.Mnemonic.STP, new DataFrame(DataType.NONE, null)));
+        var vm = new VirtualMachine(insList);
+        vm.executeAll();
+        var stack = vm.getStack();
+        assertEquals(1, stack.size());
+        var stackElement = stack.peek();
+        assertEquals(DataType.INTEGER, stackElement.type);
+        assertEquals(expectedResult, (Integer) stackElement.content);
+    }
+}
