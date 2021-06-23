698cf0a276eb65a90e3c257fa9e7205861ebf804
Khalil Santana <khalilsantana@hotmail.com>
2021-06-23 19:43:01 -0300

    ui-devel: update main icon

--- src/main/java/br/univali/comp/gui/App.java
+++ src/main/java/br/univali/comp/gui/App.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 /**
  * JavaFX App
  */
@@ -21,8 +23,8 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main.fxml"));
         Parent root = fxmlLoader.load();
         root.getStylesheets().add("/main.css");
-        primaryStage.setTitle("Compilador");
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/toolbox-48.png")));
+        primaryStage.setTitle("Compiler");
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icons/pluto.png"))));
         primaryStage.setScene(new Scene(root));
 
         Controller controller = fxmlLoader.getController();
