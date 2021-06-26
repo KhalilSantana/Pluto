afdcc821efcd521980ac43a373ec59cabed2851a
Khalil Santana <khalilsantana@hotmail.com>
2021-06-26 16:52:51 -0300

    vm-exec: add ids for DataType

--- src/main/java/br/univali/comp/virtualmachine/DataType.java
+++ src/main/java/br/univali/comp/virtualmachine/DataType.java
@@ -5,12 +5,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum DataType {
-    INTEGER,
-    FLOAT,
-    LITERAL,
-    BOOLEAN,
-    ADDRESS,
-    NONE;
+    INTEGER(1),
+    FLOAT(2),
+    LITERAL(3),
+    BOOLEAN(4),
+    ADDRESS(5),
+    NONE(0);
+
+    public final Integer id;
+
+    private DataType(Integer value) {
+        this.id = value;
+    }
+
+    public static DataType get(int id) {
+        for (DataType d : values()) {
+            if (d.id.equals(id)) {
+                return d;
+            }
+        }
+        return null;
+    }
+
 
     public static List<DataType> getNumericDataTypes() {
         return new ArrayList<>(Arrays.asList(DataType.FLOAT, DataType.INTEGER));
